ef7dbe4fe5ea9b2162d2f60c112e7f894c23c786
tejascphadnis <tejas.c.phadnis@gmail.com>
2017-03-25 06:21:33 +0000

    Working Milestone Project V1.10

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def build_graph(ticker, option, date):
     p.yaxis.axis_label_text_color = "blue" 
     p.title = "Stock Prices for: " + ticker
 #    p.title.text_color = "#4169e1"  
-    p.title.text_font_size = '18pt'
+#    p.title.text_font_size = '18pt'
 
 # based on option build graph
     if len(option) == 2:
